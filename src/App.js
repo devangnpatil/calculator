20aa39067aebc760e1623a32498be98d11a43d1e
devang <devang.patil@upgrad.com>
2019-07-20 22:57:35 +0530

    remove yarn lock

--- src/App.js
+++ src/App.js
@@ -19,42 +19,39 @@ class App extends Component {
 		num2: '',
 		output: 0,
 		active: '',
-		error: {
-			num1: '',
-			num2: ''
+		error:{
+			num1:'',
+			num2:''
 		}
 	}
 
 	handleChange = name => (e) => {
-		let value = e.target.value;
-		console.log(value)
-		if (e.target.value === "") {
-			return this.setState({ [name]: '' });
+		let value = ''
+		this.setState({ [name]: '' });
+		if(e.target.value !== ''){
+			value = parseFloat(e.target.value);
+		}else{
+			value = '';
 		}
-
-		if (!isFinite(e.target.value)) {
-			return;
-		}
-
-		value = parseFloat(e.target.value);
-		this.setState({ [name]: value, active: name });
-
+		this.setState({ active: '' });
+		
+      	this.setState({ [name]: value });
 	}
 
 	isValidAndSetError = () => {
-		let { error } = this.state;
+		let {error} = this.state;
 		let result = true;
-		Object.keys(error).forEach(item => {
-			console.log('==>', this.state[item]);
-			error[item] = isFinite(this.state[item]) ? "" : "Please Enter Value";
-			result = result && isFinite(this.state[item]);
+		Object.keys(error).forEach(item =>{
+			console.log('cause', this.state[item]);
+			error[item] = this.state[item] === "" ? "Please Enter Value":"";
+			result = result && !!this.state[item];
 		});
-		this.setState({ error });
+		this.setState({error});
 		return result;
 	}
 
 	handleClick = (operation) => {
-		if (!this.isValidAndSetError()) return;
+		if(!this.isValidAndSetError()) return;
 		this.setState({ active: operation });
 		const { num1, num2 } = this.state
 		this.props.evaluateResult({ num1, num2, operation })
@@ -64,31 +61,31 @@ class App extends Component {
 		const { num1, num2, operations, active, error } = this.state;
 		const { arithmetic } = this.props;
 		return (
-			<div className="calculator-wrapper">
-				<div className="calculator-header-wrapper">
-					<AppHeader />
+				<div className="calculator-wrapper">
+					<div className="calculator-header-wrapper">
+						<AppHeader />
+					</div>
+					<div className="input-list">
+						<input type="number"
+							value={num1}
+							onChange={this.handleChange('num1')}
+							placeholder="Enter value 1"
+						/>
+						<p className="error">{error['num1']}</p>
+						<input type="number"
+							value={num2}
+							onChange={this.handleChange('num2')}
+							placeholder="Enter value 2"
+						/>
+						<p className="error">{error['num2']}</p>
+					</div>
+					<div className="button-wrapper">
+						<CalcButton operations={operations} handleClick={this.handleClick} />
+					</div>
+					<div className="result-wrapper">
+						<CalcResult result={arithmetic.result} />
+					</div>
 				</div>
-				<div className="input-list">
-					<input type="text"
-						value={num1}
-						onChange={this.handleChange('num1')}
-						placeholder="Enter value 1"
-					/>
-					<p className="error">{error['num1']}</p>
-					<input type="text"
-						value={num2}
-						onChange={this.handleChange('num2')}
-						placeholder="Enter value 2"
-					/>
-					<p className="error">{error['num2']}</p>
-				</div>
-				<div className="button-wrapper">
-					<CalcButton operations={operations} handleClick={this.handleClick} active={active} />
-				</div>
-				<div className="result-wrapper">
-					<CalcResult result={arithmetic.result} />
-				</div>
-			</div>
 		);
 	}
 }
