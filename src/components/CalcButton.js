cb3831063fb4afddebf7e4c8b15a2ab5b6ddc559
devang <devang.patil@upgrad.com>
2019-07-20 23:12:48 +0530

    remove console

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const CalcButton = (props) => {
 	const { operations, handleClick, active } = props;
-	console.log('active', active);
 	const buttonList = operations.map((operation, index) => {
 		return (
 			<button
